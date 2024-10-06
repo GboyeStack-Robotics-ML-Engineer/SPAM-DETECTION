7fc3d5fc1e268a40d0a6b96db3a50456551fcca4
Samuel Adegboyega <gboyestack@gmail.com>
2024-10-06 23:44:56 +0100

    Adding code for Inference

--- Inference.py
+++ Inference.py
@@ -1,1 +1,12 @@
-import DataGenerator
+
+def generate(model,tokenizer,content):
+    
+    encoded_content=tokenizer(content, padding='longest', return_tensors="pt",add_special_tokens=True)
+    
+    output=model.generate(**encoded_content)
+    
+    decoded_content=tokenizer.batch_decode(output,skip_special_tokens=True)
+    
+    return " ".join(decoded_content)
+    
+    
--- app.py
+++ app.py
@@ -6,24 +6,24 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 import os
 
-# DATA_JSON_FILE = 'email-text-data.json'
-# data = pd.read_json(DATA_JSON_FILE)
+import Inference
+from Inference import generate
 
 
-# vectorizer = CountVectorizer(stop_words='english')
+import DataGenerator
+import warnings
+warnings.filterwarnings(action='ignore')
+SAVED_WEIGHTS_PATH='t5-base'#""
+SAVED_TOKENIZER_PATH=""
 
-# all_features = vectorizer.fit_transform(data.MESSAGE)
+from transformers import T5ForConditionalGeneration,T5Tokenizer
+MODEL_NAME='t5-base'
+model=T5ForConditionalGeneration.from_pretrained(SAVED_WEIGHTS_PATH)
+tokenizer=T5Tokenizer.from_pretrained(MODEL_NAME)
 
-# X_train, X_test, y_train, y_test = train_test_split(all_features, data.CATEGORY, 
-#                                                    test_size=0.3, random_state=88)
-# classifier = MultinomialNB()        
-# classifier.fit(X_train, y_train)
-
-
-def email_prediction(msg):
-    # matrix = vectorizer.transform([msg])
-
-    return msg #classifier.predict(matrix)[0]
+def email_prediction(msg,model,tokenizer):
+    
+    return  generate(model=model,tokenizer=tokenizer,content=msg)
 
 
 def main():
@@ -31,11 +31,11 @@ def main():
     st.title("SPAM e-MAIL CLASSIFICATION")
     st.subheader('Built with Python and Streamlit')
 
-    msg = st.text_input("Enter Your Message Below... eg. Let's catch up tonight, You have a meeting scheduled for tomorrow, To join our premium memebership please enter code ALLYSON30 etc.")
+    msg = st.text_input("Enter you Email Content to detect whether its HAM or SPAM")
     
     if st.button('Predict'):
         
-        result = email_prediction(msg)
+        result = email_prediction(msg,model,tokenizer)
 
         if result:
             st.error("SPAM")
