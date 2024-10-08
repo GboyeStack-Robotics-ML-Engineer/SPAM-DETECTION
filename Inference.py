574ef61978ccda353400ce85c5dd148c777210d3
Samuel Adegboyega <gboyestack@gmail.com>
2024-10-09 00:55:08 +0100

    Adding .gitignore and  Adjusting inference.py to accomodate a real word model like gemini

--- Inference.py
+++ Inference.py
@@ -1,14 +1,38 @@
 import streamlit as st
 import random
 import time
-def response_generator():
-    response = random.choice(
-        [
-            "Hello there! How can I assist you today?",
-            "Hi, human! Is there anything I can help you with?",
-            "Do you need help?",
-        ]
-    )
+import os
+import google.generativeai as genai
+
+
+import google.generativeai as genai
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+api_key=os.environ['GEMINI_API_KEY']
+# print(api_key)
+genai.configure(api_key=api_key)
+
+# Create the model
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 40,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-flash-8b",
+  generation_config=generation_config,
+  
+)
+
+
+def response_generator(session,prompt):
+    response = session.send_message(prompt)
+    response=response.text
     for word in response.split():
         yield word + " "
         time.sleep(0.05)
@@ -17,7 +41,9 @@ def response_generator():
 def app():
     # Initialize chat history
     if "messages" not in st.session_state:
-        st.session_state.messages = []
+        st.session_state.messages = [{"role": "model", 'content': 'Hey.... I am Jarvis. How can I be of assitance to you?'}]
+        
+        
 
     # Display chat messages from history on app rerun
     for message in st.session_state.messages:
@@ -32,9 +58,13 @@ def app():
         # Add user message to chat history
         st.session_state.messages.append({"role": "user", "content": prompt})
 
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        response = st.write_stream(response_generator())
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        # Display assistant response in chat message container
+    
+        history=[{"role":message['role'],"parts":[message['content'],]} for message in st.session_state.messages]
+        
+        chat_session = model.start_chat(history=history)
+        with st.chat_message("model"):
+            response = st.write_stream(response_generator(session=chat_session,prompt=prompt))
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "model", 'content': response})
 
